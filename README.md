d4a9c0b4d3d861f1c1b4262a3ee49e8f761f20dc
Lukas Eipert <git@valedictorum.de>
2014-09-23 18:28:50 +0200

    Added read me and changed API.

---
+++ README.md
@@ -0,0 +1,42 @@
+gulp-stack [![NPM version][npm-image]][npm-url] [![Dependency Status][depstat-image]][depstat-url]
+===========================
+
+A [gulp][gulp] stack for angular development.
+
+* It reduces your gulp source-code tremendously
+* Works out of the box with the Angular Best Practise app structure.
+* Is highly configurable.
+
+Installation
+---------------
+
+```shell
+npm install --save-dev gulp-stack
+```
+
+Example
+---------------
+
+Then, add it to your `gulpfile.js`:
+
+```javascript
+var gulp = require('gulp-stack').gulp();
+```
+
+API
+--------------
+
+//TODO
+
+License
+------------
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License) (c) Lukas Eipert - 2014
+
+[gulp]: https://github.com/gulpjs/gulp
+
+[npm-url]: https://npmjs.org/package/gulp-stack
+[npm-image]: https://badge.fury.io/js/gulp-stack.png
+
+[depstat-url]: https://david-dm.org/leipert/gulp-stack
+[depstat-image]: https://david-dm.org/leipert/gulp-stack.png
--- plugins.js
+++ plugins.js
@@ -2,62 +2,73 @@ var gulp = require('gulp');
 var _ = require('lodash');
 
 var through = require('through');
+module.exports = function(config) {
 
+  config = _.merge({
+    lazy: true
+  }, config);
 
-var $ = require('gulp-load-plugins')({
-  pattern: [
-    'gulp-*',
-    'main-bower-files',
-    'merge-stream',
-    'lazypipe',
-    'open'
-  ]
-});
-
-$.jsMinify = $.lazypipe()
-.pipe($.ngAnnotate)
-.pipe($.uglify)
-.pipe($.rev);
-
-$.cssMinify = $.lazypipe()
-.pipe($.autoprefixer, {
-  browsers: ['> 1%', 'last 4 versions', 'Firefox ESR', 'Opera 12.1'],
-  cascade: true
-})
-.pipe($.minifyCss, {
-  keepSpecialComments: 0
-})
-.pipe($.rev);
-
-$.htmlMinify = $.lazypipe()
-.pipe($.htmlmin, {
-  collapseBooleanAttributes: true,
-  collapseWhitespace: true,
-  removeAttributeQuotes: true,
-  removeComments: true,
-  removeEmptyAttributes: true,
-  removeRedundantAttributes: true
-});
-
-
-$.filterVinylFSByStatus = function(statuses) {
-  var count = false;
-
-  if (!_.isArray(statuses)) {
-    statuses = [statuses];
+  if (config.lazy !== false) {
+    config.lazy = true;
   }
 
-  function countFiles(file) {
-    count = count || _.contains(statuses, file.event);
-  }
+  console.log(config);
 
-  function endStream() {
-    this.emit('data', count);
-    count = false;
-    this.emit('end');
-  }
+  var $ = require('gulp-load-plugins')({
+    pattern: [
+      'gulp-*',
+      'main-bower-files',
+      'merge-stream',
+      'lazypipe',
+      'open'
+    ],
+    lazy: config.lazy
+  });
 
-  return through(countFiles, endStream);
+  $.jsMinify = $.lazypipe()
+  .pipe($.ngAnnotate)
+  .pipe($.uglify)
+  .pipe($.rev);
+
+  $.cssMinify = $.lazypipe()
+  .pipe($.autoprefixer, {
+    browsers: ['> 1%', 'last 4 versions', 'Firefox ESR', 'Opera 12.1'],
+    cascade: true
+  })
+  .pipe($.minifyCss, {
+    keepSpecialComments: 0
+  })
+  .pipe($.rev);
+
+  $.htmlMinify = $.lazypipe()
+  .pipe($.htmlmin, {
+    collapseBooleanAttributes: true,
+    collapseWhitespace: true,
+    removeAttributeQuotes: true,
+    removeComments: true,
+    removeEmptyAttributes: true,
+    removeRedundantAttributes: true
+  });
+
+
+  $.filterVinylFSByStatus = function(statuses) {
+    var count = false;
+
+    if (!_.isArray(statuses)) {
+      statuses = [statuses];
+    }
+
+    function countFiles(file) {
+      count = count || _.contains(statuses, file.event);
+    }
+
+    function endStream() {
+      this.emit('data', count);
+      count = false;
+      this.emit('end');
+    }
+
+    return through(countFiles, endStream);
+  };
+  return $;
 };
-
-module.exports = $;
